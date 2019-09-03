41e02d2af8f3d61472abd5a394c4244f6140af3b
David Lynch <DLynchContact@gmail.com>
2019-09-03 16:32:00 +0100

    Add project files.

---
+++ PoEBrowser/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PoEBrowser.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
