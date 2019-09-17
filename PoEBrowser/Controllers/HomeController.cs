9ac4e267a18cc37bbc8241c4858c371798efa23d
David Lynch <DLynchContact@gmail.com>
2019-09-17 17:17:20 +0100

    Homepage fix

--- PoEBrowser/Controllers/HomeController.cs
+++ PoEBrowser/Controllers/HomeController.cs
@@ -10,6 +10,11 @@ namespace PoEBrowser.Controllers
 {
     public class HomeController : Controller
     {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
