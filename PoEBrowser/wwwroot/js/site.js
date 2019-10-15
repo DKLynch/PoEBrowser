cb59a49d09418758f2eff32399b854df636d208d
David Lynch <DLynchContact@gmail.com>
2019-10-15 14:44:21 +0100

    Implemented Divination Card Popups
    Divination cards now show their respective card when hovering over the card inventory icon.

--- PoEBrowser/wwwroot/js/site.js
+++ PoEBrowser/wwwroot/js/site.js
@@ -1,6 +1,7 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
+// Initialize sortable tables on the page
 $(document).ready(function () {
     $("#sortableTable").tablesorter({
         theme: 'bootstrap',
@@ -20,3 +21,17 @@ $('.divCardFlavourText').each(function(){
         el.css('font-size', '10pt');
     }
 });
+
+
+// Initialize popover elements
+$('[data-toggle="popover"]').popover(
+    {
+        trigger: 'hover',
+        html: true,
+        content: function () {
+            var content = $(this).attr("data-popover-content");
+            return $(content).html();
+        },
+        template: '<div class="popover" role="tooltip"><h3 class="popover-header"></h3><div class="popover-body"></div></div>'
+    }
+)
